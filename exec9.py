4e5053056e873177959fb4c9b3e30a00f90e91b7
Flowzinnn <magebrnet@gmail.com>
2025-01-27 17:20:28 -0400

    Exec 9 - foi relativamente facil tbm

---
+++ exec9.py
@@ -0,0 +1,3 @@
+tupleList = [(9, 6, 5, 5), (10, 6, 1), (10, 4, 3, 5), (4, 3), (10, 6, 10, 4)]
+verifyMedia5 = list(filter(lambda x: x > 5,(map(lambda x: sum(x) / len(x), tupleList))))
+print(verifyMedia5) # Saída: [6.25, 5.666666666666667, 5.5, 7.5]
