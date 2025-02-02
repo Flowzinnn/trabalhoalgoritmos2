8dd3d9b3f60c3b1a4c82fec508323ec8959d1cca
Flow <magebrnet@gmail.com>
2025-02-02 15:35:03 -0400

    Update exec9.py

--- exec9.py
+++ exec9.py
@@ -1,3 +1,3 @@
 tupleList = [(9, 6, 5, 5), (10, 6, 1), (10, 4, 3, 5), (4, 3), (10, 6, 10, 4)]
-verifyMedia5 = list(filter(lambda x: x > 5,(map(lambda x: sum(x) / len(x), tupleList))))
-print(f" As médias maiores que 5 são: ",verifyMedia5) # Saída: [6.25, 5.666666666666667, 5.5, 7.5]
+verifyMedia5 = list(filter(lambda x: sum(x) / len(x) > 5, tupleList))
+print(f" As tuplas com médias maiores que 5 são: ",verifyMedia5) # Saída: [(9, 6, 5, 5), (10, 6, 1), (10, 4, 3, 5), (10, 6, 10, 4)]
